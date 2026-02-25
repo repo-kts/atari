77b2d2c84b601b37a79d46a873932217c4f8b9e8
subhash <subha11.11raj@gmail.com>
2026-02-26 02:00:44 +0530

    Removed all the unnecessary files and folders...should work clean

--- backend/scripts/testTables.js
+++
@@ -1,9 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-
-async function check() {
-    const tables = await prisma.$queryRawUnsafe(`SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'`);
-    console.log(tables.map(t => t.table_name));
-}
-
-check().catch(console.error).finally(() => prisma.$disconnect());
